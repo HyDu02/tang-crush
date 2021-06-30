1ae3a6e53c122a25c593855793c31ddb6bbe9199
HyDu02 <86713937+HyDu02@users.noreply.github.com>
2021-06-30 16:29:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     btnNo: 'Không, ml à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu. Buộc chịu và buộc iu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'lát 5h mình lại tém chung nha :)))).',
+    messDesc: 'Lát 5h mình lại tém chung nha :)))).',
     btnAccept: 'Dạ vângggggggg <3',
 }
